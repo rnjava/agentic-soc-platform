c26e10c503a8fc71b44fb22249f2704178f66b44
funnywolf <yu5890681@gmail.com>
2026-01-01 10:54:29 +0800

    update SIRP

--- PLUGINS/SIRP/CONFIG.example.py
+++ PLUGINS/SIRP/CONFIG.example.py
@@ -1,7 +1,7 @@
 # SIRP Config https://asp.viperrtp.com/asf/PLUGINS/SIRP/
 # 配置文档参考 https://asp.viperrtp.com/zh/asf/PLUGINS/SIRP/
 
-SIRP_URL = "http://192.168.241.128:8880"
+SIRP_URL = "http://192.168.241.128:8880"  # "https://www.nocoly.com" for SaaS
 SIRP_APPKEY = "d159986dbXXXXX"
 SIRP_SIGN = "M2U3M2MxZmRlYzQ3ZmM3MmI1MmJkZWYwODc4XXXXX"
-SIRP_NOTICE_WEBHOOK = "http://192.168.241.128:8880/api/workflow/hooks/NjkwMDI2Y2U1OTIxZDU4YTBhNDQxZjk5"
+SIRP_NOTICE_WEBHOOK = "http://192.168.241.128:8880/api/workflow/hooks/NjkwMDI2Y2U1OTIxZDU4YTBhNDQxZjk5"  # "https://www.nocoly.com/api/workflow/hooks/XXX" for SaaS
